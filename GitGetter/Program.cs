ac18ac2c0e65e481ec85a9c093f80e5f8b7f33a2
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-05-11 21:43:17 +0100

    Setup project with logging, etc

--- GitGetter/Program.cs
+++ GitGetter/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace GitGetter
 {
@@ -6,7 +10,39 @@ namespace GitGetter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Starting GitGetter...");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices
+                (
+                    (context, services) =>
+                    {
+                        // Config Services
+                    }
+                )
+                .UseSerilog()
+                .Build();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile
+                (
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                    true, true
+                )
+                .AddEnvironmentVariables();
         }
     }
 }
